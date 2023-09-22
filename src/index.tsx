e762a2d695745f73ae4a59bc82a122fc64a55ed8
leongaban <leongaban@gmail.com>
2023-09-22 15:18:41 -0500

    updated leon-theme package & component import

--- src/index.tsx
+++ src/index.tsx
@@ -11,9 +11,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <LeonTheme>
-      <App />
-    </LeonTheme>
+    <LeonTheme component={App} />
   </React.StrictMode>
 );
 
